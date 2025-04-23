08d1e98e4de4842f7b8dffb6d0a6a6cab1d22a8f
kou20 <kou20040428@outlook.jp>
2025-04-23 23:02:30 +0900

    env install

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+
+
 if __name__ == '__main__':
     print("Starting Flask application...")
     app.run(host='0.0.0.0', port=5000, debug=True)
