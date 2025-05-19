145bd86ea623bb83814b2ed616b794348f61607e
luyokana <kana0809kenaf@outlook.jp>
2025-05-19 22:23:22 +0900

    パスワード更新処理追加

--- ChatApp/models.py
+++ ChatApp/models.py
@@ -34,7 +34,18 @@ class User:
                 return user
           finally:
                 db_use.release(conn)
-                        
+
+     @classmethod
+     def update_password(cls, user_id, new_hashpassword):
+          conn = db_use.get_conn()
+          try:
+            with conn.cursor() as cursor:
+                sql = "UPDATE users SET password = %s WHERE user_id = %s"
+                cursor.execute(sql,(new_hashpassword, user_id))
+                conn.commit()
+          finally:
+              db_use.release(conn)
+
 
 class Login(UserMixin):
     def __init__(self, user_id):
